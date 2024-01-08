46be26ce355f498b14a476d64981e1ea2400cac7
rafly lesmana <raflylesmana111@gmail.com>
2024-01-09 05:00:42 +0700

    add privacy

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Privacy() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>Service ini hanyalah proxy dari website resmi https://pdki-indonesia.dgip.go.id/, segala kesalahan data tidak ada hubungannya detail service ini</p>
+    </main>
+  )
+}
